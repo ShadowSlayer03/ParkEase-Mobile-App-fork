a4a2dd797fa0e67328ee511d15e78d666a3039b0
Ajay-S-Biradar <sbmunnu@gmail.com>
2024-10-26 13:16:04 +0530

    sign-in sign-up completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
-import { Link } from "expo-router";
+import { useFonts } from "expo-font";
+import { Link, SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <View
       style={{
@@ -13,7 +33,7 @@ export default function Index() {
       }}
     >
       
-      <Link href={'./(home)'}>dfg</Link>
+      <Link href={'./(home)'}>Get Started > </Link>
     </View>
   );
 }
